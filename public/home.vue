406ea40986074645456fdd726418e32786319e5d
Samuelfomo <princefomo49@gmail.com>
2025-02-17 13:02:38 +0100

    pdf co

--- public/home.vue
+++ public/home.vue
@@ -45,7 +45,8 @@
       </div>
 
       <div class="flex w-full h-full py-5 justify-center flex-wrap gap-5">
-        <div v-for="(location, index) in locations" :key="index" :id="location.pdfContent" class="group h-full w-full min-h-80 max-w-sm border rounded-lg p-8 bg-blue-50  hover:bg-blue-950 hover:bg-opacity-90 group-hover:text-white hover:text-white">
+        <div v-for="(location, index) in locations" :key="index" class="group h-full w-full min-h-80 max-w-sm
+         border rounded-lg p-8 bg-blue-50  hover:bg-blue-950 hover:bg-opacity-90 group-hover:text-white hover:text-white">
           <div class="flex justify-start items-center mb-7">
             <img :src="location.icon" alt="img" class="h-20 w-20 rounded-full border-2 border-white">
           </div>
@@ -53,7 +54,7 @@
              {{location.titre}}
             </p>
             <h3 class="font-sans py-2">{{location.description}}</h3>
-          <div class="flex text-lg font-primary justify-start items-center pt-5 text-green-600 group-hover:text-white" @click="openPdf">
+          <div class="flex text-lg font-primary justify-start items-center pt-5 text-green-600 group-hover:text-white" @click="openPdf(location.pdfContent)">
             {{location.suite}}
             <svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"
                   stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round">
@@ -88,28 +89,28 @@
       </div>
     </div>
     <div class="min-h-full flex justify-between items-center w-full flex-wrap lg:px-8 px-1 bg-blue-950 pb-2">
-      <table class="w-full border-t border-green-600">
+      <table class="w-full border-t-2 border-white">
         <thead>
         <tr class="flex w-full flex-wrap justify-between lg:flex-nowrap lg:gap-0 gap-8 lg:pt-0 py-10">
-          <th class="lg:border-r justify-start flex lg:w-full w-36 lg:p-8 border-green-600">
+          <th class="lg:border-r-2 justify-start flex lg:w-full w-36 lg:p-8 border-white">
             <div class="flex flex-col lg:items-start items-center gap-1 w-full">
               <h1 class="text-4xl font-bold text-white">5M+</h1>
               <p class="text-lg text-gray-400 font-roboto">Membres</p>
             </div>
           </th>
-          <th class="lg:border-r justify-start flex lg:w-full w-44 lg:p-8 border-green-600">
+          <th class="lg:border-r-2 justify-start flex lg:w-full w-44 lg:p-8 border-white">
             <div class="flex flex-col lg:items-start items-center gap-1">
               <h1 class="text-4xl font-bold text-white">95%</h1>
               <p class="text-lg text-gray-400 font-roboto">Satisfaction des clients</p>
             </div>
           </th>
-          <th class="lg:border-r justify-start flex lg:w-full w-40 lg:p-8 border-green-600">
+          <th class="lg:border-r-2 justify-start flex lg:w-full w-40 lg:p-8 border-white">
             <div class="flex flex-col lg:items-start items-center gap-1">
               <h1 class="text-4xl font-bold text-white">73%</h1>
               <p class="text-lg text-gray-400 font-roboto">Croissance annuelle</p>
             </div>
           </th>
-          <th class="justify-start flex lg:w-full w-40 lg:p-8 border-green-600">
+          <th class="justify-start flex lg:w-full w-40 lg:p-8">
             <div class="flex flex-col lg:items-start items-center gap-1">
               <h1 class="text-4xl font-bold text-white">250B</h1>
               <p class="text-lg text-gray-400 font-roboto">Gestion des fonds</p>
@@ -124,15 +125,26 @@
         <p class="text-gray-400 text-lg py-5 w-full text-start">
           Que vous soyez une petite startup, une PME ou une grande entreprise, nous offrons des solutions financières adaptées à vos besoins :
         </p>
-
-        <div class="flex w-full h-full py-5 flex-wrap px-5 lg:px-0 gap-2">
-          <div v-for="(location4, index) in locations4" :key="index" class="flex justify-between items-start h-full min-h-44 w-full max-w-lg border rounded-lg lg:p-8 p-5 bg-blue-50">
-            <div class="border-white flex justify-center min-h-10 min-w-16 items-center">
-              <img :src="location4.icon" alt="img" class="h-16 w-16 rounded-full">
+        <div class="container mx-auto py-8 px-4 md:px-6 lg:px-0">
+          <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-4">
+            <div
+                v-for="(location4, index) in locations4"
+                :key="index"
+                class="bg-blue-50 rounded-lg shadow-md overflow-hidden transition-all duration-300 hover:shadow-lg"
+            >
+              <div class="p-6 flex space-x-4">
+                <div class="flex-shrink-0">
+                  <div class="flex w-full min-h-10 min-w-16 items-center justify-center shadow-sm">
+                    <img :src="location4.icon" alt="Icon" class="object-contain rounded-full h-16 w-16 ">
+                  </div>
+                </div>
+                <div class="flex-1">
+                  <p class="text-gray-800 text-lg font-medium leading-relaxed">
+                    {{ location4.description }}
+                  </p>
+                </div>
+              </div>
             </div>
-            <p class="px-1 text-lg font-semibold">
-              {{location4.description}}
-            </p>
           </div>
         </div>
       </div>
@@ -193,29 +205,38 @@ import Arrow from"@/assets/images/svg/arow.svg"
 import Image2 from "@public/components/image2.vue";
 import Scroll from "@public/components/scroll.vue";
 
-import pdfGenerator from '@/pdf/Pdf'
+import Pdf from "@/assets/pdf/account-conditions.pdf"
+import Pdf1 from "@/assets/pdf/request-card.pdf"
+import Pdf2 from "@/assets/pdf/start-investment.pdf"
 
-const openPdf = async () => {
+const pdf = Pdf
+
+// import pdfGenerator from '@/pdf/Pdf'
+
+const openPdf = async (pdf) => {
   try {
-    const element = document.getElementById('pdfContent')
-    // Attendre que les images soient chargées
-    // await this.$nextTick()
-    await pdfGenerator.generatePdf(element, 'rapport.pdf', true)
+    if (pdf){
+      window.open(pdf, '_blank')
+    }
+    // const element = document.getElementById('pdfContent')
+    // // Attendre que les images soient chargées
+    // // await this.$nextTick()
+    // await pdfGenerator.generatePdf(element, 'rapport.pdf', true)
   } catch (error) {
     console.error('Erreur lors de l\'ouverture du PDF:', error)
   }
 }
 
-const downloadPdf = async () => {
-  try {
-    const element = document.getElementById('pdfContent')
-    // Attendre que les images soient chargées
-    await this.$nextTick()
-    await pdfGenerator.generatePdf(element, 'rapport.pdf', false)
-  } catch (error) {
-    console.error('Erreur lors du téléchargement du PDF:', error)
-  }
-}
+// const downloadPdf = async () => {
+//   try {
+//     const element = document.getElementById('pdfContent')
+//     // Attendre que les images soient chargées
+//     await this.$nextTick()
+//     await pdfGenerator.generatePdf(element, 'rapport.pdf', false)
+//   } catch (error) {
+//     console.error('Erreur lors du téléchargement du PDF:', error)
+//   }
+// }
 
 const icon =Icon
 const arrow = Arrow
@@ -231,20 +252,21 @@ const locations = ref([
     titre:'Ouverture d\'un compte',
     description:'Simplifiez-vous la vie avec nos services personnalisés et notre soutien pour tous vos projets',
     suite:'Voir conditions',
-    pdfContent: 'pdfContent1',
+    pdfContent: Pdf,
   },
   {
     icon:icon,
     titre:'Cartes Bancaire',
     description:'Choisissez une carte bancaire qui vous offre plus de liberté, de sécurité et de fonctionnalités',
     suite:'Demander une carte',
-    pdfContent: 'pdfContent2',
+    pdfContent: Pdf1,
   },
   {
     icon:icon,
     titre:'Investissement',
     description:'Profiter d\'un suivi et d\'un accompagnement de qualité, de la définition de votre stratégie à sa mise en œuvre',
-    suite:'Démarrer un investissement'
+    suite:'Démarrer un investissement',
+    pdfContent: Pdf2,
   },
 ])
 
